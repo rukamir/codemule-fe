8e97e1a22b6cfe77f6b521c83694d211c85e5b0c
Jimmy R <jimmylroland@gmail.com>
2018-04-05 21:33:57 -0500

    Navbar selections persist when opening and closing

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,26 +1,30 @@
 <template>
     <div>
     <el-col :span="3">
-        <el-menu default-active="2" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
+        <el-menu :default-active="page.toString()" class="el-menu-vertical-demo" @open="handleOpen" @close="handleClose" :collapse="isCollapse">
         <el-button v-if=isCollapse @click="toggleCollapse" icon="el-icon-arrow-right"/>
         <el-button v-else @click="toggleCollapse" icon="el-icon-arrow-left"/>
-        <el-menu-item index="2">
+        <el-menu-item @click="setNavBar(1)" index="1">
             <i class="el-icon-upload"></i>
             <span slot="title">Upload</span>
         </el-menu-item>
-        <el-menu-item index="3">
+        <el-menu-item @click="setNavBar(2)" index="2">
             <i class="el-icon-message"></i>
             <span slot="title">Send</span>
         </el-menu-item>
-        <el-menu-item index="3">
+        <el-menu-item @click="setNavBar(3)" index="3">
             <i class="el-icon-tickets"></i>
             <span slot="title">Mailing Lists</span>
         </el-menu-item>
-        <el-menu-item index="4">
+        <el-menu-item @click="setNavBar(4)" index="4">
             <i class="el-icon-setting"></i>
             <span slot="title">Settings</span>
         </el-menu-item>
-        <el-menu-item index="4">
+        <el-menu-item @click="setNavBar(5)" index="5">
+            <i class="el-icon-star-off"></i>
+            <span slot="title">Plans</span>
+        </el-menu-item>
+        <el-menu-item @click="setNavBar(6)" index="6">
             <i class="el-icon-info"></i>
             <span slot="title">Information</span>
         </el-menu-item>
@@ -40,7 +44,8 @@
   export default {
     data() {
       return {
-        isCollapse: true
+        isCollapse: true,
+        page: 1,
       };
     },
     methods: {
@@ -52,7 +57,10 @@
       },
       toggleCollapse() {
             this.isCollapse = !this.isCollapse;
-        },
+      },
+      setNavBar(index) {
+        this.page=index;
+      }
     },
   }
 </script>
