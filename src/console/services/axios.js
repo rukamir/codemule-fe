8ca482ccddb5198c217b05508d5a096f054b84a1
Jimmy R <jimmylroland@gmail.com>
2018-09-15 18:50:14 -0500

    cleaned up axios call and formating

--- src/console/services/axios.js
+++ src/console/services/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const HOME_SERVER = process.env.VUE_APP_HOME_SERVER;
 
-let instance = axios.create({
+const instance = axios.create({
   baseURL: HOME_SERVER,
   timeout: 2000,
 });
@@ -18,19 +18,26 @@ export default {
     idToken = jwt;
   },
   get(uri) {
-    // console.log(`token ${authToken} uri ${uri}`);
-    // console.log(instance.defaults);
-    return instance.get(uri, { headers: {
-      Authorization: authToken,
-      identity: idToken,
-    }});
+    return instance.get(
+      uri,
+      {
+        headers: {
+          Authorization: authToken,
+          identity: idToken,
+        },
+      },
+    );
   },
   put(uri, body) {
-    // console.log(`token ${authToken} uri ${uri}`);
-    // console.log(instance.defaults);
-    return instance.put(uri, body, { headers: {
-      Authorization: authToken,
-      identity: idToken,
-    }});
+    return instance.put(
+      uri,
+      body,
+      {
+        headers: {
+          Authorization: authToken,
+          identity: idToken,
+        },
+      },
+    );
   },
 };
